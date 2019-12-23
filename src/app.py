24d86033b0720613a4de1f1f9588333abc4b1199
Robin Cole <robmarkcole@gmail.com>
2019-12-23 10:00:37 +0000

    Adds model files

--- src/app.py
+++ src/app.py
@@ -1,15 +1,14 @@
 import streamlit as st
+import cv2
 from PIL import Image
 import numpy as np
 
 img_file_buffer = st.file_uploader("Upload an image", type=["png", "jpg", "jpeg"])
-
-image = Image.open(img_file_buffer)
-img_array = np.array(image)
+image = np.array(Image.open(img_file_buffer))
 
 if image is not None:
     st.image(
         image,
-        caption=f"You amazing image has shape {img_array.shape[0:2]}",
+        caption=f"You amazing image has shape {image.shape[:2]}",
         use_column_width=True,
     )
