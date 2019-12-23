60033f3782d1a07fbee09b090aaf8c504e63ac9e
Robin Cole <robmarkcole@gmail.com>
2019-12-23 14:25:01 +0000

    Working processing

--- README.md
+++ README.md
@@ -10,4 +10,7 @@ From the root dir:
 Then visit [localhost:8501](http://localhost:8501/)
 
 ## Development
-Using [devcontainer](https://code.visualstudio.com/docs/remote/containers), see basic python [example repo](https://github.com/microsoft/vscode-remote-try-python) and [more advanced repo](https://github.com/microsoft/python-sample-tweeterapp). Use [this streamlit docker template](https://github.com/MrTomerLevi/streamlit-docker). Note you cannot docker run at the same time as the devcontainer as the ports clash. 
+Using [devcontainer](https://code.visualstudio.com/docs/remote/containers), see basic python [example repo](https://github.com/microsoft/vscode-remote-try-python) and [more advanced repo](https://github.com/microsoft/python-sample-tweeterapp). Use [this streamlit docker template](https://github.com/MrTomerLevi/streamlit-docker). Note you cannot docker run at the same time as the devcontainer as the ports clash.
+
+## References
+I took a lot of inspiration from [this article](https://www.pyimagesearch.com/2017/09/11/object-detection-with-deep-learning-and-opencv/) by Adrian Rosebrock
--- src/app.py
+++ src/app.py
@@ -4,22 +4,62 @@ from PIL import Image
 import numpy as np
 import os
 
+from const import CLASSES, COLORS
+from settings import DEFAULT_CONFIDENCE_THRESHOLD, DEMO_IMAGE, MODEL, PROTOTXT
+
+
+def process_image(image):
+    blob = cv2.dnn.blobFromImage(
+        cv2.resize(image, (300, 300)), 0.007843, (300, 300), 127.5
+    )
+    net = cv2.dnn.readNetFromCaffe(PROTOTXT, MODEL)
+    net.setInput(blob)
+    detections = net.forward()
+    return detections
+
+
+def annotate_image(
+    image, detections, confidence_threshold=DEFAULT_CONFIDENCE_THRESHOLD
+):
+    # loop over the detections
+    (h, w) = image.shape[:2]
+    labels = []
+    for i in np.arange(0, detections.shape[2]):
+        confidence = detections[0, 0, i, 2]
+
+        if confidence > confidence_threshold:
+            # extract the index of the class label from the `detections`,
+            # then compute the (x, y)-coordinates of the bounding box for
+            # the object
+            idx = int(detections[0, 0, i, 1])
+            box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+            (startX, startY, endX, endY) = box.astype("int")
+
+            # display the prediction
+            label = f"{CLASSES[idx]}: {round(confidence * 100, 2)}%"
+            labels.append(label)
+            cv2.rectangle(image, (startX, startY), (endX, endY), COLORS[idx], 2)
+            y = startY - 15 if startY - 15 > 15 else startY + 15
+            cv2.putText(
+                image, label, (startX, y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, COLORS[idx], 2
+            )
+    return image, labels
+
+
 img_file_buffer = st.file_uploader("Upload an image", type=["png", "jpg", "jpeg"])
 
 if img_file_buffer is not None:
     image = np.array(Image.open(img_file_buffer))
-    st.image(
-        image,
-        caption=f"You amazing image has shape {image.shape[:2]}",
-        use_column_width=True,
-    )
 
 else:
-    demo_image = "images/demo.jpg"
+    demo_image = DEMO_IMAGE
     image = np.array(Image.open(demo_image))
-    st.image(
-        image, caption=f"This is the demo image", use_column_width=True,
-    )
 
-blob = cv2.dnn.blobFromImage(cv2.resize(image, (300, 300)), 0.007843, (300, 300), 127.5)
-st.write(f"blob.shape : {blob.shape}")
+detections = process_image(image)
+image, labels = annotate_image(image, detections)
+st.write(labels)
+
+st.image(
+    image, caption=f"Processed image", use_column_width=True,
+)
+
